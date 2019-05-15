f1fdddd3c7587eb88646c194345c38cb227372b6
Felix <felix.delrio.1@gmail.com>
2019-05-15 19:38:47 +0200

    Funcionalidad boton SIguiente
    Felix y Luis en DIscord

--- js/ventanaCiclos.js
+++ js/ventanaCiclos.js
@@ -1,41 +1,53 @@
- // Evento de clic para cambiar el modulo activo
- $(".contenedorModulo").click(function () {
+// Evento de clic para cambiar el modulo activo
+$(".contenedorModulo").click(function () {
     $(".contenedorModulo").not("contenedorActivo").removeClass("contenedorActivo").css({ "pointer-events": "auto" })
     $(this).addClass("contenedorActivo").css({ "pointer-events": "none" })
 });
 
- // Evento de hover si esta activo el modulo
- $(".contenedorModulo").css("color", "black").hover(function () {
+// Evento de hover si esta activo el modulo
+$(".contenedorModulo").css("color", "black").hover(function () {
     !($(this).hasClass("contenedorActivo"))
         ? $(this).addClass("contenedorActivohover").css("color", "black")
         : $(this).css("color", "black")
 }, function () {
-    $(this).removeClass("contenedorActivohover").css("color","black");
+    $(this).removeClass("contenedorActivohover").css("color", "black");
 });
 
 
 $(document).ready(function () {
 
     // Funcionalidad con la API
-    $(".contenedorModulo:eq(0)").click(() => { matriculaCiclos("DAM"); console.log("DAM")})
-    $(".contenedorModulo:eq(1)").click(() => { matriculaCiclos("DAW"); console.log("DAW")})
-    $(".contenedorModulo:eq(2)").click(() => { matriculaCiclos("ASIR"); console.log("ASIR")})
+    $(".contenedorModulo:eq(0)").click(() => { console.log("DAM") })
+    $(".contenedorModulo:eq(1)").click(() => { console.log("DAW") })
+    $(".contenedorModulo:eq(2)").click(() => { console.log("ASIR") })
+
+    // Boton Siguiente Click
+    $("#btnSiguiente").click(() => {
+        if ($(".contenedorModulo:eq(0)") == $(this)) {
+            localStorage.setItem("ciclo", "DAM");
+        } else if ($(".contenedorModulo:eq(1)") == $(this)) {
+            localStorage.setItem("ciclo", "DAW");
+        } else if ($(".contenedorModulo:eq(2)") == $(this)) {
+            localStorage.setItem("ciclo", "ASIR");
+        } else {
+            alert("Picha escogeme un ciclo, arme el favo")
+        }
+    })
 });
 
 //Funcion Matricula Ciclos
 function matriculaCiclos(familiaProfesional) {
-   
+
     var uri = "https://proyecto-mdc-api.herokuapp.com/crearMatricula";
     console.log("URI: " + uri)
 
-    $.post(uri, { familiaProfesional: familiaProfesional})
-   
+    $.post(uri, { familiaProfesional: familiaProfesional })
+
     $("#btnSiguiente").removeAttr("disabled");
 
-    $("#btnSiguiente").mouseover(function(){
+    $("#btnSiguiente").mouseover(function () {
         $(this).css("background-color", "rgb(7, 148, 14)")
-    }).mouseout(function() {
+    }).mouseout(function () {
         $(this).css("background-color", "rgb(131, 153, 166)")
     });;
-
 }
