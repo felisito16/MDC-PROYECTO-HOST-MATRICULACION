2f61749e8844a094d8e58569c6e638c88a22a375
Luis <a>
2019-06-16 19:26:12 +0200

    Nueva Version 8

--- js/ventanaDocumentacion.js
+++ js/ventanaDocumentacion.js
@@ -1,9 +1,13 @@
 $(document).ready(() => {
+    
+    botonHabilitar($("#btnSiguiente"));
+    
     for(var i = 4;i<6;i++)
     {
         bNavegacionDeshabilitar(i)
     }
 })
+
 
 $("#btnSiguiente").click(() => {
    
--- js/ventanaFormaPago.js
+++ js/ventanaFormaPago.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
 
-        bNavegacionDeshabilitar(5)
+    botonHabilitar($("#btnSiguiente"));
+    bNavegacionDeshabilitar(5)
     
 })
 
